1006beb22ef8504bec528e2edba0295a1d10a96e
fede53 <federicogermi@hotmail.com>
2023-01-26 17:09:19 +0100

    Regions, Walks and WalksDifficulty Model and Migration

---
+++ Cube/Cube.API/Data/CubeWalksContext.cs
@@ -0,0 +1,17 @@
+﻿using Cube.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cube.API.Data
+{
+    public class CubeWalksContext: DbContext
+    {
+        public CubeWalksContext(DbContextOptions<CubeWalksContext> options): base(options) 
+        { 
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
